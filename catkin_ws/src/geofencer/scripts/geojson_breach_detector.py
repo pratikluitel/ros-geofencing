e3426bf19e39640b209b0e27f3e4e9edd9b7c8b6
tyr1ant <p2anmol@gmail.com>
2020-12-02 14:53:37 +0545

    changed breach detector to detect multiple polygons, added parkscene.geojson for use with parkscene testbed

--- catkin_ws/src/geofencer/scripts/geojson_breach_detector.py
+++ catkin_ws/src/geofencer/scripts/geojson_breach_detector.py
@@ -9,32 +9,28 @@ from std_msgs.msg import String, Float32MultiArray, Empty
 pub = rospy.Publisher('/geofence_status', String, queue_size=100)
 #flag = 0
 zfence = (-1, 5.5)
-polygon = Polygon()
-
+polys = []
 
 def callback(data):
     #print("Data received, processing")
     gps = data.data
     print(gps)
     p = Point(gps[0], gps[1])
-    if not p.within(polygon) or gps[2] < zfence[0] or gps[2] > zfence[1]:
+
+    for poly in polys:
+        if p.within(poly):
+            print("breached")
+            pub.publish("breached")
+            return
+
+    if gps[2] < zfence[0] or gps[2] > zfence[1]:
         print("breached")
         pub.publish("breached")
-        '''
-        diff = Float32MultiArray()
-        xneg = min(0, gps[0]-xfence[0])
-        xpos = max(0, gps[0]-xfence[1])
-        yneg = min(0, gps[1]-yfence[0])
-        ypos = max(0, gps[1]-yfence[1])
-        zneg = min(0, gps[2]-zfence[0])
-        zpos = max(0, gps[2]-zfence[1])
-        diff.data = [ -xneg if not xpos else -xpos, -yneg if not ypos else -ypos, -zneg if not zpos else -zpos ]
-        pub.publish(diff)
-        '''
-    else:
-        print("not breached")
-        pub.publish(" ")
-        pass
+        return
+    
+    print("not breached")
+    pub.publish(" ")
+    pass
 
 
 def listener():
@@ -55,8 +51,7 @@ if __name__ == '__main__':
 
     for feature in data["features"]:
         if feature["geometry"]["type"] == "Polygon":
-            coords = feature["geometry"]["coordinates"]
+            polys.append(Polygon(feature["geometry"]["coordinates"]))
 
-    polygon = Polygon(coords)
     print("Node running")
     listener()
