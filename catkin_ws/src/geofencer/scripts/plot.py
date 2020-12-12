111916c412600bf828239200a488c33e5e779831
Pratik Luitel <pluitel11@gmail.com>
2020-12-12 12:48:57 +0545

    changes to plot file

--- catkin_ws/src/geofencer/scripts/plot.py
+++ catkin_ws/src/geofencer/scripts/plot.py
@@ -25,23 +25,50 @@ def listener():
     while not rospy.is_shutdown():
         rate.sleep()
 
+def plot_traj(x,y,t,mask,axs,col='blue'):
+    xb =[]
+    yb =[]
+    tb =[]
+
+    st = t[mask][0]
+    prev_i=st-1
+    for i in t[mask]:
+        if i-prev_i>1 or i ==t[mask][-1]:
+            xb.append(x[st:prev_i+2])
+            yb.append(y[st:prev_i+2])
+            tb.append(t[st:prev_i+2])
+            st=i
+            prev_i=st-1
+        prev_i+=1
+
+    for j in range(len(tb)):
+        axs[0].plot(tb[j],xb[j],col)
+        axs[1].plot(tb[j],yb[j],col)
+        axs[2].plot(xb[j],yb[j],col)
+
+
 points = []
 breaches = []
 col='blue'
 time=0
 listener()
+
+#plotting
 mask = np.array(breaches) == ' '
-fig, axs = plt.subplots(2)
-fig.suptitle('x and y coordinates vs time plot')
+fig, axs = plt.subplots(3)
 x = np.array([point[0] for point in points])
 y = np.array([point[1] for point in points])
-t = np.array(range(time))*0.1 #rate of sampling is 100ms
-axs[0].plot(t[mask],x[mask],'blue')
-axs[0].plot(t[np.logical_not(mask)],x[np.logical_not(mask)],'red')
-axs[0].set(ylabel='x')
+t = np.array(range(time))
 
-axs[1].plot(t[mask],y[mask],'blue')
-axs[1].plot(t[np.logical_not(mask)],y[np.logical_not(mask)],'red')
+plot_traj(x,y,t,mask,axs)
+plot_traj(x,y,t,np.logical_not(mask),axs,'red')
+
+axs[0].set(ylabel='x')
 axs[1].set(ylabel='y')
+axs[2].set(ylabel='y')
+
+axs[0].set(xlabel='time (sec)')
 axs[1].set(xlabel='time (sec)')
+axs[2].set(xlabel='x')
+
 plt.show()
