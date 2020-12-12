8eb30900612ee11c13c58bb509f3c611dd3e2019
Pratik Luitel <pluitel11@gmail.com>
2020-12-12 13:23:24 +0545

    improved plotting

--- catkin_ws/src/geofencer/scripts/plot.py
+++ catkin_ws/src/geofencer/scripts/plot.py
@@ -36,7 +36,7 @@ def plot_traj(x,y,t,mask,axs,col='blue'):
         if i-prev_i>1 or i ==t[mask][-1]:
             xb.append(x[st:prev_i+2])
             yb.append(y[st:prev_i+2])
-            tb.append(t[st:prev_i+2])
+            tb.append(t[st:prev_i+2]*0.1)
             st=i
             prev_i=st-1
         prev_i+=1
@@ -55,20 +55,24 @@ listener()
 
 #plotting
 mask = np.array(breaches) == ' '
-fig, axs = plt.subplots(3)
+ax1 = plt.subplot2grid((2,3),(0,0))
+ax3 = plt.subplot2grid((2,3),(0,1),rowspan=2,colspan=2)
+ax2 = plt.subplot2grid((2,3),(1,0))
+axs = [ax1,ax2,ax3]
+
+ax1.set(xlabel='time (sec)',ylabel='x')
+ax2.set(xlabel='time (sec)',ylabel='y')
+ax3.set(xlabel='x',ylabel='y')
+
+ax1.grid()
+ax2.grid()
+ax3.grid()
+
 x = np.array([point[0] for point in points])
 y = np.array([point[1] for point in points])
 t = np.array(range(time))
 
 plot_traj(x,y,t,mask,axs)
 plot_traj(x,y,t,np.logical_not(mask),axs,'red')
-
-axs[0].set(ylabel='x')
-axs[1].set(ylabel='y')
-axs[2].set(ylabel='y')
-
-axs[0].set(xlabel='time (sec)')
-axs[1].set(xlabel='time (sec)')
-axs[2].set(xlabel='x')
 
 plt.show()
